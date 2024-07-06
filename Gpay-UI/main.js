bef94bd336b516603a9f89e193c5d4f731dadc9d
S-Anbu <anbarasans750@gmail.com>
2024-07-06 21:04:12 +0530

    down button added

--- Gpay-UI/main.js
+++ Gpay-UI/main.js
@@ -22,3 +22,9 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+const dropdownButton = document.querySelector("#dropdown");
+const dropdownList = document.querySelector("#dropdown + div.hidden");
+
+dropdownButton.addEventListener("click", () => {
+  dropdownList.classList.toggle("hidden");
+});
