e75347094e47b88fff93ed6cbe96eaf977928bbf
S-Anbu <anbarasans750@gmail.com>
2024-07-07 19:38:18 +0530

    page overflow functionality added

--- Gpay-UI/main.js
+++ Gpay-UI/main.js
@@ -3,28 +3,3 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
-const dropdownButton = document.querySelector("#dropdown");
-const dropdownList = document.querySelector("#dropdown + div.hidden");
-
-dropdownButton.addEventListener("click", () => {
-  dropdownList.classList.toggle("hidden");
-});
--- Gpay-UI/tailwind.config.js
+++ Gpay-UI/tailwind.config.js
@@ -1,13 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
-}
-
+    module.exports = {
+      purge: {
+        enabled: false,
+        content: ["./index.html",
+    "./**/*.{js,ts,jsx,tsx}"]
+      },
+      darkMode: 'class', // or 'media' or 'class'
+      theme: {
+        extend: {},
+      },
+      variants: {
+        extend: {
+          display: ['dark'],
+          opacity: ['dark'],
+        },
+      },
+      plugins: [],
+    }
+    
